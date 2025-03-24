fca0ae445788881cd727c337a98bc113635e2800
ViktorKula <vicyru@yandex.ru>
2025-03-24 20:47:07 +0300

    add API

--- publicposts/publicposts/urls.py
+++ publicposts/publicposts/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from publicpostsapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/posts/', views.posts),
+    path('api/like_post/<int:post_id>/', views.like_post),
 ]
